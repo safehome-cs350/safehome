828f1d370b3dd4dadc69a034a99264043c9734fe
Sihun Chae <sihchae@gmail.com>
2025-11-20 14:26:50 +0900

    Divide API directories (#6)

--- backend/app.py
+++ backend/app.py
@@ -1,48 +1,9 @@
 """API for SafeHome System."""
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+
+from .common import router as common_router
 
 app = FastAPI(title="SafeHome API", version="1.0")
 
-
-class LoginRequest(BaseModel):
-    """Login Request Model."""
-
-    user_id: str
-    password1: str
-    password2: str
-
-
-users_db = {"homeowner1": {"password1": "12345678", "password2": "abcdefgh"}}
-
-
-@app.post(
-    "/login/",
-    summary="UC1.b. Log onto the system through web browser.",
-    responses={
-        401: {
-            "description": "Invalid user ID or password",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "string",
-                    },
-                }
-            },
-        }
-    },
-)
-def login(login_request: LoginRequest):
-    """UC1.b. Log onto the system through web browser."""
-    user = users_db.get(login_request.user_id)
-    if not user:
-        raise HTTPException(status_code=401, detail="ID not recognized")
-
-    if (
-        login_request.password1 != user["password1"]
-        or login_request.password2 != user["password2"]
-    ):
-        raise HTTPException(status_code=401, detail="Password incorrect")
-
-    return "Welcome!"
+app.include_router(common_router)
---
+++ backend/common/__init__.py
@@ -0,0 +1,5 @@
+"""Common."""
+
+from .common import router
+
+__all__ = ["router"]
