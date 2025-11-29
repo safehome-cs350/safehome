b77f22f9660299a459bcc5736a62f928ebbcadac
Sihun Chae <sihchae@gmail.com>
2025-11-29 19:28:17 +0900

    Add common integration tests

--- tests/integration_tests/test_common.py
+++ tests/integration_tests/test_common.py
@@ -5,6 +5,7 @@ from unittest.mock import Mock, patch
 
 import pytest
 
+from control_panel.control_panel import ControlPanel, ControlPanelState
 from frontend.configuration_panel import ConfigurationPanel
 from frontend.login_window import LoginWindow
 
@@ -15,6 +16,22 @@ def tk_root():
     root = tk.Tk()
     yield root
     root.destroy()
+
+
+def test_uc_1a():
+    """Test for UC1.a. Log onto the system through control panel."""
+    # 1. The homeowner/guest uses the control panel.
+    control_panel = ControlPanel()
+
+    # 2. The homeowner/guest enters master/guest password. (4 digits password)
+    # 3. The system validates password.
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+
+    # 4. The system shows accessible functions on the control panel.
+    assert control_panel.state == ControlPanelState.MASTER
 
 
 def test_uc_1b(tk_root):
@@ -53,3 +70,89 @@ def test_uc_1c(tk_root):
 
     # 6
     mock_showinfo.assert_called_once_with("Success", "Settings saved successfully")
+
+
+def test_uc_1d():
+    """Test for UC1.d. Turn the system on."""
+    control_panel = ControlPanel()
+
+    # Login
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.MASTER
+
+    # Power off
+    control_panel.button2()
+    assert control_panel.state == ControlPanelState.POWERED_OFF
+
+    # Power on
+    control_panel.button1()
+    assert control_panel.state == ControlPanelState.IDLE
+
+
+def test_uc_1e():
+    """Test for UC1.e. Turn the system off."""
+    control_panel = ControlPanel()
+
+    # Login
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.MASTER
+
+    # Power off
+    control_panel.button2()
+    assert control_panel.state == ControlPanelState.POWERED_OFF
+
+
+def test_uc_1f():
+    """Test for UC1.f. Reset the system."""
+    control_panel = ControlPanel()
+
+    # Login
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.MASTER
+
+    # Reset
+    control_panel.button3()
+    assert control_panel.state == ControlPanelState.IDLE
+
+
+def test_uc_1g():
+    """Test for UC1.g. Change master password through control panel."""
+    control_panel = ControlPanel()
+
+    # Login
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.MASTER
+
+    # Change password
+    control_panel.button6()
+    assert control_panel.state == ControlPanelState.PASSWORD_CHANGE_CURRENT
+
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.PASSWORD_CHANGE_NEW
+
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.PASSWORD_CHANGE_RECONFIRM
+
+    control_panel.button1()
+    control_panel.button2()
+    control_panel.button3()
+    control_panel.button4()
+    assert control_panel.state == ControlPanelState.MASTER
